eda319e99388412a5f64fb48a900e51f989f2bab
Konstantin <106445847+kzaraliev@users.noreply.github.com>
2025-05-12 14:18:11 +0300

    Hide spinner

--- app/layout.js
+++ app/layout.js
@@ -100,7 +100,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={roboto.className}>
-        <NextTopLoader />
+        <NextTopLoader showSpinner={false}/>
         <ImagePreloader />
         <Navigation />
         <main>{children}</main>
